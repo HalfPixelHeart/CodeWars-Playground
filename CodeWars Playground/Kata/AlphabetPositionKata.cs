e8846aa40a43ec1792773ce639e83cb040ccef43
Andrew Alexander <andy.alex27@gmail.com>
2022-03-25 09:34:35 +0000

    Added challenge requirements to each class for reference.

--- CodeWars Playground/Kata/AlphabetPositionKata.cs
+++ CodeWars Playground/Kata/AlphabetPositionKata.cs
@@ -1,4 +1,8 @@
-﻿namespace CodeWars_Playground.Kata
+﻿// In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+// If anything in the text isn't a letter, ignore it and don't return it.
+// "a" = 1, "b" = 2, etc.
+
+namespace CodeWars_Playground.Kata
 {
     public static class AlphabetPositionKata
     {
--- CodeWars Playground/Kata/TwoToOne.cs
+++ CodeWars Playground/Kata/TwoToOne.cs
@@ -1,4 +1,6 @@
-﻿
+﻿// Take 2 strings, s1 and s2, including only letters from a to z.
+// Return a new sorted string, containing only distinct letters from s1 and s2.
+
 namespace CodeWars_Playground.Kata
 {
     public static class TwoToOne
