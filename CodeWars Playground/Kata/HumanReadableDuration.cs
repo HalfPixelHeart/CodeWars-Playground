4eb5fbe1ac0037f70f7d9dc9f84c8760f02a110f
Andrew Alexander <andy.alex27@gmail.com>
2022-03-30 11:50:15 +0100

    Fixed bug in HumanReadableDuration

--- CodeWars Playground/Kata/HumanReadableDuration.cs
+++ CodeWars Playground/Kata/HumanReadableDuration.cs
@@ -10,14 +10,13 @@ namespace CodeWars_Playground.Kata
 
             var periods =  new Stack<TimePeriod>();
             var delimiters = new Stack<string>(new string[] { ", ", ", ", ", ", " and ", ""});
-            var outputList = new List<string>();
             var output = "";
 
-            if (seconds > 31536000) periods.Push(new TimePeriod("year", 31536000, ref seconds));
-            if (seconds > 86400) periods.Push(new TimePeriod("day", 86400, ref seconds));
-            if (seconds > 3600) periods.Push(new TimePeriod("hour", 3600, ref seconds));
-            if (seconds > 60) periods.Push(new TimePeriod("minute", 60, ref seconds));
-            if (seconds > 1) periods.Push(new TimePeriod("second", 1, ref seconds));
+            if (seconds >= 31536000) periods.Push(new TimePeriod("year", 31536000, ref seconds));
+            if (seconds >= 86400) periods.Push(new TimePeriod("day", 86400, ref seconds));
+            if (seconds >= 3600) periods.Push(new TimePeriod("hour", 3600, ref seconds));
+            if (seconds >= 60) periods.Push(new TimePeriod("minute", 60, ref seconds));
+            if (seconds >= 1) periods.Push(new TimePeriod("second", 1, ref seconds));
 
             while(periods.Count > 0) output = periods.Pop().formatted + delimiters.Pop() + output;
 
