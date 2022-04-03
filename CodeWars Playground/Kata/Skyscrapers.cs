cd1403280cd89089f22f240ad571061d98bfbc61
Andrew Alexander <andy.alex27@gmail.com>
2022-04-03 13:04:09 +0100

    Ceated Skyscraper Kata

---
+++ CodeWars Playground/Kata/Skyscrapers.cs
@@ -0,0 +1,28 @@
+﻿
+
+namespace CodeWars_Playground.Kata
+{
+    public class Skyscrapers
+    {
+
+    }
+
+    public struct VantagePoint
+    {
+        public int[] View = new int[4];
+        public int MaxVisibility;
+        public int Visibility
+        {
+
+        }
+
+        public VantagePoint(int[] view, int maxVisibility)
+        {
+            view.CopyTo(View, 0);
+            MaxVisibility = maxVisibility;
+        }
+
+
+    }
+
+}
